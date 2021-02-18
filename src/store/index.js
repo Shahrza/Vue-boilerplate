4a023ac9cdebd05244dfb198fa8cf7f963e122be
Shahrza <smuqbilli@gmail.com>
2021-02-18 07:41:46 -0800

    state modules added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import sample from './modules/sample'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    sample
+  },
   state: {
   },
   mutations: {
